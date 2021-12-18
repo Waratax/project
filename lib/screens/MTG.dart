94c17b101d514b037b8aa774fd95d94c25470310
Zdenek Pospisil <zdenek.pospisil@foxyco.cz>
2021-12-18 16:14:59 +0100

    add mode picker non bug, could be better

--- lib/screens/MTG.dart
+++ lib/screens/MTG.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'dart:convert';
-import 'package:flutter_picker/flutter_picker.dart';
 import 'dart:math';
 
 class MTGScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _MTGScreen();
 }
 
-const PickerData = '''[
-["1","2","3","4","5"
-],
-["6","4","8"]
-    ]''';
-
 class _MTGScreen extends State<MTGScreen> {
-  var playerLife1;
-  var playerLife2;
-  var gameMode = "1";
-
-  void setMode() {
-    setState(() {
-      if (gameMode == "2") {
-        playerLife1 = 40;
-        playerLife2 = 40;
-      } else if (gameMode == "3") {
-        playerLife1 = 30;
-        playerLife2 = 30;
-      } else {
-        playerLife1 = 20;
-        playerLife2 = 20;
-      }
-    });
-  }
+  var playerLife1 = 20;
+  var playerLife2 = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -47,31 +23,6 @@ class _MTGScreen extends State<MTGScreen> {
               Row(
                 children: [
                   Spacer(),
-                  Container(
-                      child: Transform(
-                          transform: Matrix4.rotationX(pi),
-                          alignment: Alignment.center,
-                          child: Expanded(
-                              child: Align(
-                            alignment: FractionalOffset.center,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    playerLife1++;
-                                  });
-                                },
-                                child: Text("+"),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.green,
-                                )),
-                          )))),
-                  Spacer(),
-                  Container(
-                      child: Transform(
-                          transform: Matrix4.rotationX(pi),
-                          alignment: Alignment.center,
-                          child: Text(playerLife1.toString()))),
-                  Spacer(), //life
                   Container(
                       child: Transform(
                     transform: Matrix4.rotationX(pi),
@@ -91,21 +42,95 @@ class _MTGScreen extends State<MTGScreen> {
                           )),
                     )),
                   )),
+                  Spacer(),
+                  Container(
+                      child: Transform(
+                          transform: Matrix4.rotationX(pi),
+                          alignment: Alignment.center,
+                          child: Transform(
+                              transform: Matrix4.rotationY(pi),
+                              alignment: Alignment.center,
+                              child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(playerLife1.toString()))))),
+                  Spacer(),
+                  Container(
+                      child: Transform(
+                          transform: Matrix4.rotationX(pi),
+                          alignment: Alignment.center,
+                          child: Expanded(
+                              child: Align(
+                            alignment: FractionalOffset.center,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    playerLife1++;
+                                  });
+                                },
+                                child: Text("+"),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.green,
+                                )),
+                          )))), //life
                   Spacer()
                 ],
               ),
               Spacer(),
-              ElevatedButton(
+              Container(
+                child: ElevatedButton(
+                  child: Text("settings"),
                   onPressed: () {
-                    showPicker(context);
+                    showDialog(
+                        context: context,
+                        builder: (_) => AlertDialog(
+                              insetPadding: EdgeInsets.all(2),
+                              title: Text('MODE'),
+                              content: Text('pick mode to play'),
+                              actions: [
+                                Row(children: [
+                                  Container(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          playerLife1 = 20;
+                                          playerLife2 = 20;
+                                        });
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("standard"),
+                                    ),
+                                    padding: EdgeInsets.all(2),
+                                  ),
+                                  Container(
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            playerLife1 = 30;
+                                            playerLife2 = 30;
+                                          });
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text("modern")),
+                                    padding: EdgeInsets.all(2),
+                                  ),
+                                  Container(
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            playerLife1 = 40;
+                                            playerLife2 = 40;
+                                          });
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text("commander")),
+                                    padding: EdgeInsets.all(2),
+                                  ),
+                                ])
+                              ],
+                            ));
                   },
-                  child: Text("Settings")),
-              ElevatedButton(
-                  onPressed: () {
-                    setMode();
-                  },
-                  child: Text(
-                      "tempbutton")), //TODO rework tempbutton -> to pickerbutton
+                ),
+              ),
               Spacer(),
               Row(
                 children: [
@@ -144,17 +169,5 @@ class _MTGScreen extends State<MTGScreen> {
             ],
           ),
         ));
-  }
-
-  showPicker(BuildContext context) {
-    new Picker(
-        adapter: PickerDataAdapter<String>(
-            pickerdata: new JsonDecoder().convert(PickerData), isArray: true),
-        hideHeader: true,
-        title: new Text("Mode"),
-        onConfirm: (Picker picker, List value) {
-          gameMode = picker.getSelectedValues()[0];
-          print(picker.getSelectedValues());
-        }).showDialog(context);
   }
 }
