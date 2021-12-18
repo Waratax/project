29b29c1d30f9f8986a6647571a326b55ac826035
Zdenek Pospisil <zdenek.pospisil@foxyco.cz>
2021-12-18 10:37:44 +0100

    add mode select (with bugs)

--- lib/screens/MTG.dart
+++ lib/screens/MTG.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:convert';
+import 'package:flutter_picker/flutter_picker.dart';
 import 'dart:math';
 
 class MTGScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _MTGScreen();
 }
 
+const PickerData = '''[
+["1","2","3","4","5"
+],
+["6","4","8"]
+    ]''';
+
 class _MTGScreen extends State<MTGScreen> {
-  var playerLife1 = 20;
-  var playerLife2 = 20;
+  var playerLife1;
+  var playerLife2;
+  var gameMode = "1";
+
+  void setMode() {
+    setState(() {
+      if (gameMode == "2") {
+        playerLife1 = 40;
+        playerLife2 = 40;
+      } else if (gameMode == "3") {
+        playerLife1 = 30;
+        playerLife2 = 30;
+      } else {
+        playerLife1 = 20;
+        playerLife2 = 20;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +46,7 @@ class _MTGScreen extends State<MTGScreen> {
               Spacer(),
               Row(
                 children: [
+                  Spacer(),
                   Container(
                       child: Transform(
                           transform: Matrix4.rotationX(pi),
@@ -39,11 +65,13 @@ class _MTGScreen extends State<MTGScreen> {
                                   primary: Colors.green,
                                 )),
                           )))),
+                  Spacer(),
                   Container(
                       child: Transform(
                           transform: Matrix4.rotationX(pi),
                           alignment: Alignment.center,
-                          child: Text(playerLife1.toString()))), //life
+                          child: Text(playerLife1.toString()))),
+                  Spacer(), //life
                   Container(
                       child: Transform(
                     transform: Matrix4.rotationX(pi),
@@ -62,15 +90,22 @@ class _MTGScreen extends State<MTGScreen> {
                             primary: Colors.red,
                           )),
                     )),
-                  ))
+                  )),
+                  Spacer()
                 ],
               ),
               Spacer(),
               ElevatedButton(
                   onPressed: () {
-                    Navigator.pop(context); //TODO modes
+                    showPicker(context);
                   },
                   child: Text("Settings")),
+              ElevatedButton(
+                  onPressed: () {
+                    setMode();
+                  },
+                  child: Text(
+                      "tempbutton")), //TODO rework tempbutton -> to pickerbutton
               Spacer(),
               Row(
                 children: [
@@ -109,5 +144,17 @@ class _MTGScreen extends State<MTGScreen> {
             ],
           ),
         ));
+  }
+
+  showPicker(BuildContext context) {
+    new Picker(
+        adapter: PickerDataAdapter<String>(
+            pickerdata: new JsonDecoder().convert(PickerData), isArray: true),
+        hideHeader: true,
+        title: new Text("Mode"),
+        onConfirm: (Picker picker, List value) {
+          gameMode = picker.getSelectedValues()[0];
+          print(picker.getSelectedValues());
+        }).showDialog(context);
   }
 }
