39e6fa9a677b5436f2d45286d7d1b7dd00e2f9a8
Zdenek Pospisil <zdenek.pospisil@foxyco.cz>
2022-03-19 12:45:15 +0100

    visual update of mainScr

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -18,57 +18,87 @@ class _MainScreenState extends State<MainScreen> {
         body: Center(
             child: Column(
           children: [
-            Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/MTG"); //magic the gathering
-                  },
-                  child: Text("Play MTG"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                  )),
+            Spacer(),
+            Row(
+              children: [
+                Spacer(),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, "/MTG"); //magic the gathering
+                      },
+                      child: Text("MTG Life counter"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueAccent.shade200,
+                      )),
+                ),
+                Spacer(),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/diceRoll"); //dice rolls
+                      },
+                      child: Text("Roll dice"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueAccent.shade200,
+                      )),
+                ),
+                Spacer(),
+              ],
             ),
-            Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/diceRoll"); //dice rolls
-                  },
-                  child: Text("Play DiceRoll"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                  )),
+            Spacer(),
+            Row(
+              children: [
+                Spacer(),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, "/diceGame"); //dice the game
+                      },
+                      child: Text("Play dice game"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueAccent.shade200,
+                      )),
+                ),
+                Spacer(),
+                Container(
+                  width: 150,
+                  height: 150,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context,
+                            "/liarsDice"); //liars dice the dice game #2
+                      },
+                      child: Text("Board game"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueAccent.shade200,
+                      )),
+                ),
+                Spacer(),
+              ],
             ),
+            Spacer(),
             Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/diceGame"); //dice the game
-                  },
-                  child: Text("Play dice the game"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                  )),
-            ),
-            Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(
-                        context, "/liarsDice"); //liars dice the dice game #2
-                  },
-                  child: Text("Play liars dice"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                  )),
-            ),
-            Container(
+              width: 150,
+              height: 150,
               child: ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, "/cards"); //card game for sure
                   },
-                  child: Text("Play cards"),
+                  child: Text("Chess timer"),
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
+                    primary: Colors.blueAccent.shade200,
                   )),
             ),
+            Spacer(),
           ],
         )));
   }
